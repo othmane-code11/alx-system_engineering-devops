0df1bfa674018bbfdab07113e0feb51d11636c84
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-04-19 12:55:09 -0700

    config_manag

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
+package {'werkseug':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
 package {'flask':
-  ensure   => '2.1.1',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
